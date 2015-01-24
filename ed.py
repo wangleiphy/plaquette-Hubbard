aa0c3bacc7cdd943eb93a474bc9af0a23a96df49
Lei Wang <wangleiphy@gmail.com>
2015-01-24 10:07:21 +0100

    initial commit

---
+++ ed.py
@@ -0,0 +1,46 @@
+import pyalps
+from subprocess import check_call 
+
+L=2
+W=2
+Nup = 3 
+Ndn = 3 
+t0 = 1.0
+t1 = 0.5 
+U = 2.0 
+
+parms = []
+for t1 in [0.1, 0.5, 1.0]:
+    parms.append({
+          'LATTICE_LIBRARY'           : 'mylattices.xml',
+          'MODEL_LIBRARY'             : 'mymodels.xml',
+   	      'LATTICE'                   : "plaquette lattice", 
+          'MODEL'                     : "fermion Hubbard",
+	      'CONSERVED_QUANTUMNUMBERS'  : 'Nup, Ndown',
+ 	      'TRANSLATION_SYMMETRY'      : 'true',
+          'L'                         : L,
+          'W'                         : W,
+          't0'                        : t0,  
+          't1'                        : t1,  
+          'U'                         : U,  
+	      'Nup_total'                 : Nup, 
+	      'Ndown_total'               : Ndn, 
+          'NUMBER_EIGENVALUES'        : 1, 
+          'BCx'                       : "periodic",
+          'BCy'                       : "periodic"
+#          'MEASURE_CORRELATIONS[corr]': 'cdag:c',
+#          'MEASURE_CORRELATIONS[nncorr]': 'n:n',
+#          'INITIAL_SITE'              : 0
+          #"PRINT_EIGENVECTORS"        : 1
+        })
+
+
+input_file = pyalps.writeInputFiles('plaquette',parms)
+#res = pyalps.runApplication('sparsediag',input_file,writexml=False)
+
+for p in parms:
+    parmname = str(p['LATTICE']).replace(" ", "")+'L'+str(p['L'])+'_W'+str(p['W'])+'_Nup'+str(p['Nup_total'])+'_Ndn'+str(p['Ndown_total']) +'_t1'+str(p['t1']) +'_U'+str(p['U'])
+    input_file = pyalps.writeInputFiles(parmname, [p])
+    pyalps.runApplication('sparsediag',input_file) #,writexml=True)#,MPI=2)
+#    check_call(['bsub','-oo',input_file.replace('.in.xml','.log'),'-W','08:00','sparsediag',input_file])
+    #check_call(['bsub', '-n', '5','-oo',input_file.replace('.in.xml','.log'),'-W','08:00','mpirun', 'sparsediag', '--mpi', input_file])
---
+++ energy.py
@@ -0,0 +1,42 @@
+import pyalps
+import matplotlib.pyplot as plt
+import pyalps.plot
+import numpy as np
+import re
+import sys
+
+if __name__=='__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-fileheader", default='plaquettelatticeL2_W2_Nup3_Ndn3', help="fileheader")
+    parser.add_argument("-dir", default='./', help="dir")
+    args = parser.parse_args()
+
+    resfiles = pyalps.getResultFiles(dirname= args.dir, prefix=args.fileheader)
+    resfiles.sort() 
+    #print resfiles 
+
+    data = pyalps.loadSpectra(resfiles)
+
+    #print data,len(data)
+
+    for sim in data:
+
+        L = int(sim[0].props['L'])
+        U = sim[0].props['U']
+        t1 = sim[0].props['t1']
+        
+        # for all momentum sector 
+        all_energies = []
+        for sec in sim:
+            all_energies += list(sec.y)
+
+        index = np.argsort(all_energies)
+        print L, U, t1, all_energies[index[0]] 
+
+        #print "#kx, ky, Energy"
+        # for all momentum sector 
+        #for sec in sim:
+        #    kx, ky =  sec.props['TOTAL_MOMENTUM'].split() 
+        #    print "{:10.4f}".format(float(kx)/np.pi),  "{:10.4f}".format(float(ky)/np.pi), sec.y
